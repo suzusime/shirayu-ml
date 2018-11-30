71b7372c8b89d266ec78304718e7e7ee7ecca78a
suzusime <tamagawajousuiro+git@gmail.com>
2018-11-30 23:51:25 +0900

    main.ml を追加

---
+++ main.ml
@@ -0,0 +1,21 @@
+open Syntax
+open Eval
+
+let rec read_eval_print env =
+  print_string "# ";
+  flush stdout;
+  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+  let (id, newenv, v) = eval_decl env decl in
+  Printf.printf "val %s = " id;
+  pp_val v;
+  print_newline();
+  read_eval_print newenv
+
+let initial_env =
+  Environment.enpty
+  |> Environment.extend "i" (IntV 1)
+  |> Environment.extend "v" (IntV 5)
+  |> Environment.extend "x" (IntV 10)
+
+let _ = read_eval_print initial_env
+
