c860cc8710fb2f470a98b26a39a4f29dc861b5cf
suzusime <tamagawajousuiro+git@gmail.com>
2018-12-01 00:50:56 +0900

    実行時の型エラーで落ちないようにした (ex. 3.2.2)

--- main.ml
+++ main.ml
@@ -5,11 +5,16 @@ let rec read_eval_print env =
   print_string "# ";
   flush stdout;
   let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_decl env decl in
-  Printf.printf "val %s = " id;
-  pp_val v;
-  print_newline();
-  read_eval_print newenv
+  try
+    let (id, newenv, v) = eval_decl env decl in
+    Printf.printf "val %s = " id;
+    pp_val v;
+    print_newline();
+    read_eval_print newenv
+  with
+    Error e ->
+    print_endline ("[Error] " ^ e);
+    read_eval_print env
 
 let initial_env =
   Environment.empty
