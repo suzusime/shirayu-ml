4952ea5ec243b9e60fbc874e61d96361c72fee43
suzusime <tamagawajousuiro+git@gmail.com>
2018-12-02 06:50:46 +0900

    各種二項演算子とifの型推論を実装

--- typing.ml
+++ typing.ml
@@ -12,20 +12,45 @@ let ty_prim op ty1 ty2 = match op with
     (match ty1, ty2 with
        TyInt, TyInt -> TyInt
      | _ -> err ("Argument must be of integer: +"))
-  | _ -> err ("Not Implemented!")
+  | Mult ->
+    (match ty1, ty2 with
+       TyInt, TyInt -> TyInt
+     | _ -> err ("Argument must be of integer: *"))
+  | Lt ->
+    (match ty1, ty2 with
+       TyInt, TyInt -> TyBool
+     | _ -> err ("Argument must be of integer: <"))
+  | Or ->
+    (match ty1, ty2 with
+       TyBool, TyBool -> TyBool
+     | _ -> err ("Argument must be of boolean: ||"))
+  | And ->
+    (match ty1, ty2 with
+       TyBool, TyBool -> TyBool
+     | _ -> err ("Argument must be of boolean: &&"))
 
 let rec ty_exp tyenv = function
     Var x ->
     (try
        Environment.lookup x tyenv
      with
-       Environment.Not_bound -> err ("variable not bound: " ^ x))
+       Environment.Not_bound -> err ("Variable not bound: " ^ x))
   | ILit _ -> TyInt
   | BLit _ -> TyBool
   | BinOp (op, exp1, exp2) ->
     let tyarg1 = ty_exp tyenv exp1 in
     let tyarg2 = ty_exp tyenv exp2 in
     ty_prim op tyarg1 tyarg2
+  | IfExp (t, e1, e2) ->
+    let tytest = ty_exp tyenv t in
+    let tye1 = ty_exp tyenv e1 in
+    let tye2 = ty_exp tyenv e2 in
+    (match tytest with
+       TyBool ->
+       if tye1 = tye2 then tye1 else err ("Types of results must agree: if")
+     | _ -> err ("Test expression must have type boolean: if")
+    )
+  | LetExp (id, exp1, exp2) -> err ("Not Implemented!")
   | _ -> err ("Not Implemented!")
 
 let ty_decl tyenv = function
